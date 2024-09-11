4cd0397d893b43a4c486d7888ec7d5b1081c2b9a
ViktorOmelchenko <viktor432102@gmail.com>
2024-09-11 14:19:58 +0300

    some work on access and minor fixes

--- TStraining/src/handlers/addToGroupForce.ts
+++ TStraining/src/handlers/addToGroupForce.ts
@@ -1,10 +1,8 @@
 import { joinGroup } from "../InnerData/GetData";
-import { accessSession } from "../InnerData/sessionControl";
 
 async function addToGroup(req: any, res: any, next: Function) {
     try {
-        const session = accessSession(req.cookies.sessionId)
-        const userId = session.id
+        const userId = req.body.userId
         const groupId = req.body.groupId
         await joinGroup(userId, groupId)
         res.end('done')
